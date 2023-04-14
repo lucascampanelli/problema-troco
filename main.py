3bf0cb6e980ca0f1b9eb86938162424f90712d9c
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2023-04-14 01:38:30 -0300

    Separação de exercícios

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from maior18 import *;
-from calcMedia import *;
 from troco import *;
 from trocoCorrigido import *;
-
-# Exercício 3 - Lista 1
-#maior18();
-
-# Exercício 1 - Lista 2
-#calcMedia();
 
 # Exercício Guloso - Devolver troca
 #obterTroco(76);
 
-# Exercício Guloso - Correção do Professor
+# Exercício Guloso - Sugestão de resolução do Professor
 troco(130);
